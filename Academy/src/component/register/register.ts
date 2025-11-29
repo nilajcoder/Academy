8798473611637845ba82c5bb60e2300fa8e5fdd0
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-11-29 23:55:02 +0530

    registermatform

--- Academy/src/component/register/register.ts
+++ Academy/src/component/register/register.ts
@@ -3,11 +3,24 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-register',
-   standalone: true,
-  imports: [RouterModule ,CommonModule, FormsModule ],
+  imports: [
+    RouterModule ,
+    CommonModule,
+     FormsModule,
+     MatFormFieldModule,
+     
+     MatInputModule,
+     MatIconModule
+
+
+   ],
+   
   templateUrl: './register.html',
   styleUrls: ['./register.css'],
 })
@@ -16,13 +29,13 @@ export class Register {
 
   fullname: string = '';
     email: string = '';
-    username: string = '';
+    mobile: string = '';
     password: string = '';
 
     onRegister() {
         console.log("Full Name:", this.fullname);
         console.log("Email:", this.email);
-        console.log("Username:", this.username);
+        console.log("Username:", this.mobile);
         console.log("Password:", this.password);
     }
 
